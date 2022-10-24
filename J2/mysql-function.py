b9ed248383dc2fe6b5e652280b36c52baaa66d29
pvig-ynov <pierre.vigie@ynov.com>
2022-10-24 23:20:27 +0200

    création de la fonction et roadmap

--- J2/mysql-function.py
+++ J2/mysql-function.py
@@ -1,23 +1,34 @@
-import mysql.connector
-from mysql.connector import Error
+# import mysql.connector
+# from mysql.connector import Error
 
-try:
-    connection = mysql.connector.connect(host='51.159.207.166',
-                                         database='',
-                                         user='admin',
-                                         password='Azerty77*')
-    if connection.is_connected():
-        db_Info = connection.get_server_info()
-        print("Connected to MySQL Server version ", db_Info)
-        cursor = connection.cursor()
-        cursor.execute("select database();")
-        record = cursor.fetchone()
-        print("You're connected to database: ", record)
+# try:
+#     connection = mysql.connector.connect(host='51.159.207.166',
+#                                          database='',
+#                                          user='admin',
+#                                          password='Azerty77*')
+#     if connection.is_connected():
+#         db_Info = connection.get_server_info()
+#         print("Connected to MySQL Server version ", db_Info)
+#         cursor = connection.cursor()
+#         cursor.execute("select database();")
+#         record = cursor.fetchone()
+#         print("You're connected to database: ", record)
 
-except Error as e:
-    print("Error while connecting to MySQL", e)
-finally:
-    if connection.is_connected():   
-        cursor.close()
-        connection.close()
-        print("MySQL connection is closed")
+# except Error as e:
+#     print("Error while connecting to MySQL", e)
+# finally:
+#     if connection.is_connected():   
+#         cursor.close()
+#         connection.close()
+#         print("MySQL connection is closed")
+
+
+
+def handle(event, context):
+    return {
+        "body": {
+            "message": 'Hello, world',
+        },
+        "statusCode": 200,
+    }
+      
