a5807a064fd097903b6b18ec1619e5f797064734
Pvig-Ynov <pierre@QDX-2020033014.Equadex.local>
2022-10-27 14:31:26 +0200

    création des groupes de sécu & ip pub

--- J3/main.tf
+++ J3/main.tf
@@ -1,3 +1,29 @@
+
+# Création du private network
+
+resource "scaleway_vpc_private_network" "pvig_priv" {
+    name = "pvig_subnet"
+}
+# création des l'IP publiques des instances
+
+resource "scaleway_instance_ip" "pvig_ins_ip" {}
+
+
+# Création du security group
+
+resource "scaleway_instance_security_group" "pvig_sg" {
+  inbound_default_policy  = "drop" # By default we drop incoming traffic that do not match any inbound_rule.
+  outbound_default_policy = "accept" # By default we drop outgoing traffic that do not match any outbound_rule.
+
+  inbound_rule {
+    action = "accept"
+    port   = 22
+    ip     = var.auth_ip
+  }
+}
+
+# création de l'instance rdb
+
 resource "scaleway_rdb_instance" "main" {
   name           = "pvig-rdb"
   node_type      = "DB-DEV-S"
@@ -8,20 +34,27 @@ resource "scaleway_rdb_instance" "main" {
   password       = "Azerty77!"
 }
 
+# création de la base
+
 resource "scaleway_rdb_database" "main" {
   instance_id    = scaleway_rdb_instance.main.id
   name           = "my-new-database"
 }
 
+# création du volume de l'instance web
+
 resource "scaleway_instance_volume" "data" {
-  size_in_gb     = 100
+  size_in_gb     = 30
   type           = "b_ssd"
 }
+
+# création de l'instance web
 
 resource "scaleway_instance_server" "web" {
   type           = "DEV1-S"
   image          = "ubuntu_jammy"
   name           = "pvig-ins"
+  ip_id = scaleway_instance_ip.pvig_ins_ip.id
 
   root_volume {
     delete_on_termination = false
